8ea1f3646d242ebd4433023250ea7503990c6669
josueCastillejos <211234@ids.upchiapas.edu.mx>
2023-04-11 12:07:07 -0600

    get and set

--- src/main/java/com/basedatos/basededatos/models/FamiliarModel.java
+++ src/main/java/com/basedatos/basededatos/models/FamiliarModel.java
@@ -26,4 +26,44 @@ public class FamiliarModel {
 
     @Column(name = "Status")
     private String Status;
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+    public String getType_Familiar() {
+        return Type_Familiar;
+    }
+
+    public void setType_Familiar(String type_Familiar) {
+        Type_Familiar = type_Familiar;
+    }
+
+    public String getBirth_Date() {
+        return Birth_Date;
+    }
+
+    public void setBirth_Date(String birth_Date) {
+        Birth_Date = birth_Date;
+    }
+
+    public String getLast_Name() {
+        return Last_Name;
+    }
+
+    public void setLast_Name(String last_Name) {
+        Last_Name = last_Name;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
 }
